64795fab990562b7686a706aa2352897d539faa0
dave <dave@metabets.vip>
2024-11-07 16:19:38 +0800

    initial commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,72 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <section className="w-full h-[70vh] md:h-[90vh] items-center flex justify-center  background">
+        <div className="flex flex-col content-center text-center bg-[#F0F1EB] p-10 border-4 border-black">
+          <div className="text-6xl mb-4 font-cursive">Kyu</div>
+          <div className="text-3xl">is</div>
+          <div className="text-[#529DB8] text-8xl md:text-[10rem] font-extrabold space tracking-widest scale-y-150 mt-4 md:mt-7">
+            ONE
+          </div>
+          {/* <div className="text-3xl">November 30th, 2024</div> */}
+        </div>
+      </section>
+      <section className="w-full items-center flex justify-center bg-[#E9B82C] border-y-4 border-black">
+        <div className="bg-[#F0F1EB] h-[70vh] my-32 w-[800px] border-4 border-black mx-3">
+          <div className="items-center flex flex-col justify-center w-[300px] mx-auto my-20 gap-7 text-center relative">
+            {/* <img src="./cake1.png" className="w-60 absolute top-[-200px]"/> */}
+            <div className="text-2xl font-mono leading-snug pt-10">
+              Please join us to celebrate Kyu's 1st Birthday
+            </div>
+            <div>
+              When
+              <hr className="w-[20px] h-0.5 bg-black mx-auto my-1" />
+              Sep 26, 2035, 10:00 AM
+            </div>
+            <div>
+              Where
+              <hr className="w-[20px] h-0.5 bg-black mx-auto my-1" />
+              Dylan's 1st Birthday, 500 Terry A Francois Blvd, San Francisco, CA
+              94158, USA
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full h-full bg-[#529DB8]">
+        <div className="flex flex-col justify-center w-[300px] mx-auto gap-7 text-center pb-10">
+          <div className="text-2xl font-semibold leading-snug mt-10 bg-[#F0F1EB] border-4 border-black">
+            For Direction
+          </div>
+          <div className="mapouter border-4 border-black">
+            <div className="gmap_canvas">
+              <iframe
+                className="gmap_iframe"
+                width="100%"
+                height="385"
+                frameBorder="0"
+                scrolling="no"
+                src="https://maps.google.com/maps?width=771&height=385&hl=en&q=Janella’s Private Pool&t=&z=14&ie=UTF8&iwloc=B&output=embed"
+              ></iframe>
+              <a
+                href="https://sprunkin.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Sprunki Incredibox
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '"Inter"',
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        cursive: ['"Dancing Script"', 'cursive'],
+        long: ['"Playfair Display"', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
